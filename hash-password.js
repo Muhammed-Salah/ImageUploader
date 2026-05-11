b4ba9866411dcf96ec03a83cccd39704700b82f3
Muhammed-Salah <19gcs01@meaec.edu.in>
2026-05-11 11:33:53 +0530

    Cleanup

--- hash-password.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-const password = 'MHM@786';
-const salt = bcrypt.genSaltSync(10);
-const hash = bcrypt.hashSync(password, salt);
-console.log('PASSWORD_HASH:', hash);
